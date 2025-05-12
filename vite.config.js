0609a85744a61afd38b11bdd66aca99629a62aa1
Vishaal-G <vishaaltor@gmail.com>
2025-05-12 19:26:25 -0400

    Got rid of file path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    build: {
-        rollupOptions: {
-            input: path.resolve(__dirname, 'src/main.jsx'), // or 'src/main.js' depending on your file
-        },
-    },
 });
