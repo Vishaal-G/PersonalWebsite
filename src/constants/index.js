31c5c8f2e4f5e3fff98d3bad4bd0562bc8faec4f
Vishaal-G <vishaaltor@gmail.com>
2025-05-21 18:55:23 -0400

    Cleaned up code

--- src/constants/index.js
+++ src/constants/index.js
@@ -128,20 +128,6 @@ const expCards = [
     },
 ];
 
-const expLogos = [
-    {
-        name: "logo1",
-        imgPath: "/images/logo1.png",
-    },
-    {
-        name: "logo2",
-        imgPath: "/images/logo2.png",
-    },
-    {
-        name: "logo3",
-        imgPath: "/images/logo3.png",
-    },
-];
 
 const socialImgs = [
     {
@@ -158,7 +144,6 @@ export {
     words,
     counterItems,
     expCards,
-    expLogos,
     socialImgs,
     techStackIcons,
     techStackImgs,
